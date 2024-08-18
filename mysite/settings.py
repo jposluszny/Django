198a10150ee870690e88043b90fa8d6ee8189021
jp <wektor78@gmail.com>
2024-08-18 17:24:59 +0200

    Pythonanywhere mysql database was set

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,22 +75,26 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'postgres',
-    #     'USER': 'ja',
-    #     'PASSWORD': 'q',
-    #     'HOST': 'postgres_db',
-    #     'PORT': 5432,
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysqldb',
+        'USER': 'ja',
+        'PASSWORD': 'password123',
+        'HOST': 'localhost',
+        'PORT': 3306,
+    },
+    #   'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'wektor78$default',
+    #     'USER': 'wektor78',
+    #     'PASSWORD': 'Mojehaslodomysql',
+    #     'HOST': 'localhost',
+    #     'PORT': 3306,
     # },
-        
-
-
-    
 }
 
 
